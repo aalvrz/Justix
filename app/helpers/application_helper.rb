5843760f3b2caea4b6c6bcaa941b16f819f0f711
Jaime Alvarez <jaime_andres@unitec.edu>
2015-11-11 09:33:11 +0000

    Fix registration issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    
+    def nav_link(link_text, link_path)
+        content_tag(:li, class: ('current' if link_path == url_for(only_path: true)) ) do
+            link_to link_text, link_path
+        end
+    end
 end
