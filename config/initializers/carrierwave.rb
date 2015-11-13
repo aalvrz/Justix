19d876f4ff4995e36422423588410cd6b2694b4d
Jaime Alvarez <jaime_andres@unitec.edu>
2015-11-13 02:45:31 +0000

    try no aws creds

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,9 +12,9 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     #config.storage = :s3
     config.fog_credentials = {
-        :provider               => 'AWS',
-        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+        #:provider               => 'AWS',
+        #:aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+        #:aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
         #:region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
     }
     config.fog_directory  = "justix" # Bucket
