e8162324c98f97042bea00f34dbb738b1c685a47
Jaime Alvarez <jaime_andres@unitec.edu>
2015-11-13 02:51:46 +0000

    fix carrierwave fog conf

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,12 +10,12 @@ end
 # Configure to use Amazon AWS for Production
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    #config.storage = :s3
     config.fog_credentials = {
         :provider               => 'AWS',
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-        :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
+        #:region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
     }
     config.fog_directory  = "justix" # Bucket
   end
