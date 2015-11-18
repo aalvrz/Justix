a50c8602dabb6595836787e1458e0287b2afeb44
Jaime Alvarez <jaime_andres@unitec.edu>
2015-11-18 03:23:01 +0000

    Configure Sendgrid mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  # Configure SMTP Mailer with SendGrid
+  config.action_mailer.default_url_options = { host: 'http://mibufete.herokuapp.com' }
   
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :domain               => 'heroku.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain,
+    :enable_starttls_auto => true  }
   
 end
