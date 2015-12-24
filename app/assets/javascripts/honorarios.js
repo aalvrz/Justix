45496dcabfa3999b2eebeaa0251aac4169ea403b
Jaime Alvarez <jaime_andres@unitec.edu>
2015-12-24 03:21:36 +0000

    Fix datepicker for Honorarios

---
+++ app/assets/javascripts/honorarios.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.datetimepicker').datetimepicker({
+        icons: {
+                    time: "fa fa-clock-o",
+                    date: "fa fa-calendar",
+                    up: "fa fa-arrow-up",
+                    down: "fa fa-arrow-down",
+                    previous: 'fa fa-arrow-left',
+                    next: 'fa fa-arrow-right'
+                }
+    });
+});
