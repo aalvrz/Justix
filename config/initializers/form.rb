06b9020e5861f5d21e4a77e98ef6ad56ccc571a3
Jaime Alvarez <jaime_andres@unitec.edu>
2015-12-24 06:44:29 +0000

    Change date_select formatted_date format

--- config/initializers/form.rb
+++ config/initializers/form.rb
@@ -5,7 +5,7 @@ module ActionView
         
       def date_select(method, options = {}, html_options = {})
         existing_date = @object.send(method) 
-        formatted_date = existing_date.to_date.strftime("%F") if existing_date.present?
+        formatted_date = existing_date.to_date.strftime("%d %m %Y") if existing_date.present?
         @template.content_tag(:div, :class => "input-group") do    
           text_field(method, :value => formatted_date, :class => "form-control datepicker", :"data-date-format" => "DD/MM/YYYY") +
           @template.content_tag(:span, @template.content_tag(:span, "", :class => "fa fa-calendar") ,:class => "input-group-addon")
