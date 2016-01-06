009a86c9fb0aa301c511aa00021e4e06b6b9c89c
Jaime Alvarez <jaime_andres@unitec.edu>
2016-01-06 08:52:50 +0000

    Fix creating Personas from Caso through AJAX calls with Modal

--- app/assets/javascripts/casos.js
+++ app/assets/javascripts/casos.js
@@ -34,6 +34,19 @@ $(function () {
         hintText: "Introduzca el nombre del testigo...",
         noResultsText: "No hay resultados",
         searchingText: "Buscando..."
-    });    
+    });
+    
+    $('#new_persona_modal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var type = button.data('ptype'); // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('#persona_type').val(type); // Change the select box value
+    });
+    
+    $(".modal").on("hidden.bs.modal", function(){
+        $(this).find('form')[0].reset();
+    });
 
 });
